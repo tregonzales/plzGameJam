6ea43dd1915e9335b1ef1c30c296ebcdaee478ca
Tre Gonzales <Tre@education.com>
2018-02-08 16:51:02 +0100

    took out weird variable

--- Assets/Scripts/BennoController.cs
+++ Assets/Scripts/BennoController.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 public class Benno : MonoBehaviour {
 
   public float speed;
-  public AudioSource nomz;
 
   void Update() {
     transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;

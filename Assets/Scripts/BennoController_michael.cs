b56906a158f9ba45a1f77a478eec1d1a79c72dbe
Michael Gonzalez <mikegonz@udel.edu>
2018-02-09 10:30:43 +0100

    fixed stuff

--- Assets/Scripts/BennoController_michael.cs
+++ Assets/Scripts/BennoController_michael.cs
@@ -3,41 +3,29 @@ using UnityEngine;
 
 using System.Collections;
 
-public class BennoController : MonoBehaviour {
+public class BennoController_michael : MonoBehaviour {
 
-    public float speed = 10, jumpVelocity = 10;
-    Transform myTrans;
-    Rigidbody2D myBody;
+    public float speed = 10, torque = 0;
+	Rigidbody2D rigidbody;
 
     void Start()
     {
-        //  myBody = this.rigidbody2D;//Unity 4.6-
-        myBody = this.GetComponent<Rigidbody2D>();//Unity 5+
-        myTrans = this.transform;
+        rigidbody = this.GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate()
     {
 
         Move(Input.GetAxisRaw("Horizontal"));
-        if (Input.GetButtonDown("Jump"))
-            Jump();
     }
 
-    void Move(float horizonalInput)
+    void Move(float horizontalInput)
     {
-
-  void Update() {
-		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
-		if (movement.x != 0)
+		if (horizontalInput != 0)
 			Debug.Log (movement.x);
 		rigidbody = gameObject.GetComponent<Rigidbody2D> ();
 		rigidbody.AddTorque (torque);
 	}
 
-    public void Jump()
-    {
-        myBody.velocity += jumpVelocity * Vector2.up;
-    }
 
 }
---
+++ Assets/Scripts/BennoController_someone.cs
@@ -0,0 +1,39 @@
+
+using UnityEngine;
+
+using System.Collections;
+
+public class BennoController : MonoBehaviour {
+
+    public float speed = 10, jumpVelocity = 10;
+    Transform myTrans;
+    Rigidbody2D myBody;
+
+    void Start()
+    {
+        //  myBody = this.rigidbody2D;//Unity 4.6-
+        myBody = this.GetComponent<Rigidbody2D>();//Unity 5+
+        myTrans = this.transform;
+    }
+
+    void FixedUpdate()
+    {
+
+        Move(Input.GetAxisRaw("Horizontal"));
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    void Move(float horizonalInput)
+    {
+        Vector2 moveVel = myBody.velocity;
+        moveVel.x = horizonalInput * speed;
+        myBody.velocity = moveVel;
+    }
+
+    public void Jump()
+    {
+        myBody.velocity += jumpVelocity * Vector2.up;
+    }
+
+}

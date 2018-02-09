00d74d643aacca0ced33056be3da3ab1f1255ed5
Michael Gonzalez <mikegonz@udel.edu>
2018-02-09 11:06:11 +0100

    fixed error

--- Assets/Scripts/BennoController_michael.cs
+++ Assets/Scripts/BennoController_michael.cs
@@ -8,6 +8,9 @@ public class BennoController_michael : MonoBehaviour {
     public float speed = 10, torque = 0;
 	Rigidbody2D rigidbody;
 
+	private bool canGrab = false;
+	private bool holding = false;
+
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody2D>();
@@ -15,7 +18,11 @@ public class BennoController_michael : MonoBehaviour {
 
     void FixedUpdate()
     {
-
+		if (Input.GetKeyDown(KeyCode.D)){
+			if (holding) {
+				transform.DetachChildren();
+			}
+		}
         Move(Input.GetAxisRaw("Horizontal"));
     }
 
@@ -27,5 +34,20 @@ public class BennoController_michael : MonoBehaviour {
 		}
 	}
 
+	void OnCollisionEnter2D(Collision2D other){
+		canGrab = true;
+	}
+
+	void OnCollisionStay2D(Collision2D coll) {
+		if (coll.gameObject.CompareTag("grabbable")) {
+			if (Input.GetKeyDown(KeyCode.Space)){
+				if (canGrab) {
+					coll.gameObject.transform.SetParent(gameObject.transform);
+					holding = true;
+					canGrab = false;
+				}
+			}
+		}
+	}
 
 }

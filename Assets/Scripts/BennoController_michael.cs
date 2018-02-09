f76d2a72944a6b658a792e7ffbb3991c55ea8e3e
Michael Gonzalez <mikegonz@udel.edu>
2018-02-09 10:23:57 +0100

    fixed stuff i think

--- Assets/Scripts/BennoController_michael.cs
+++ Assets/Scripts/BennoController_michael.cs
@@ -10,7 +10,9 @@ public class BennoController_Mike : MonoBehaviour {
 	public float torque = 0.1f;
 
   void Update() {
-    transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
+		Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
+		if (movement.x != 0)
+			Debug.Log (movement.x);
 		rigidbody = gameObject.GetComponent<Rigidbody2D> ();
 		rigidbody.AddTorque (torque);
   }
